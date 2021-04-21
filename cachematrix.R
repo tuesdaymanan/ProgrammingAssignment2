3f9f4f23ac516cb6ae2c4f9f6e2bed6406a8944a
Tuesday Cecille T. Manan <tctmanan@mymail.mapua.edu.ph>
2021-04-21 15:33:05 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## partial fulfillment is used to write these functions
+## there are a total of two functions that are used namely makeCacheMatrix and cacheSolve.
 
-## Write a short comment describing this function
+## in the first function, the inverse of the function is needed to be cached.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  SetInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = get inverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is used in the second function
+## this function is utilized to solve the special matrix's inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    messaage("getting cached data")
+    return(inv)
+  }
+  mat <- x$get(
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
 }
